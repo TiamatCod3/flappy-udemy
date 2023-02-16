eb21c36776cbf2437dad12ac0637ea7c3f62c858
TiamatCod3 <ti_matos@id.uff.br>
2023-02-16 00:28:38 -0300

    Setup inicial

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,14 @@
 import {defineConfig} from 'vite'
+
+export default defineConfig({
+    base: '/flappy-udemy/',
+    build: {
+        // Do not inline images and assets to avoid the phaser error
+        // "Local data URIs are not supported"
+        assetsInlineLimit: 0,
+        chunkSizeWarningLimit: 1600,
+    },
+    server:{
+        port: 3000
+    }
+})
